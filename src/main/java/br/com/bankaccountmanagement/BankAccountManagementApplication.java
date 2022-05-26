c9e225fa65edd592682c13a524ac42bf31c3b838
Kleryton <kleryton.dev@gmail.com>
2022-05-25 22:45:13 -0300

    Created project

---
+++ src/main/java/br/com/bankaccountmanagement/BankAccountManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.bankaccountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bankaccountmanagement/BankAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bankaccountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
