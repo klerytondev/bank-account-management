b4f5c2aa956dbf87ed10d9f0d29097242ac80208
Kleryton <kleryton.dev@gmail.com>
2022-05-27 15:12:04 -0300

    Created classes InternacionalizacaoConfig and SwaggerConfig

---
+++ src/main/java/br/com/bankaccountmanagement/configs/InternacionalizacaoConfig.java
@@ -0,0 +1,36 @@
+package br.com.bankaccountmanagement.configs;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+/*
+* Esta classe é responsável por permitir que o arquivo 
+* message.properties possa ser utilizado. Ela configura o nome do arquivo
+* e o local geográfico onde essse arquivo será usado.
+*/
+
+@Configuration
+public class InternacionalizacaoConfig {
+	
+	@Bean
+	public MessageSource MessageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("ISO-8859-1");
+		messageSource.setDefaultLocale(Locale.getDefault());
+		return messageSource;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean validatorFactoryBean() {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(MessageSource());
+		return bean;
+	}
+
+}
---
+++ src/main/java/br/com/bankaccountmanagement/configs/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package br.com.bankaccountmanagement.configs;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/*
+ * Esta classe é reponsável por configurar e habilitar o Swagger na aplicação
+ * 
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	  @Bean
+	    public Docket api() {
+	        return new Docket(DocumentationType.SWAGGER_2)
+	                .select()
+	                .apis(RequestHandlerSelectors.basePackage("br.com.kleryton.bankingsystem"))
+	                .paths(PathSelectors.any())
+	                .build()
+	                .apiInfo(showMetadata());
+	    }
+
+	    private ApiInfo showMetadata() {
+	        return new ApiInfoBuilder()
+	                .title("Bank Account Management - API Restfull")
+	                .description("Bank Account Management")
+	                .version("1.0")
+	                .license("Apache License Version 2.0")
+	                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+	                .contact(new Contact("Kleryton de Souza", "https://github.com/kleryton.dev/", "klerytondev@gmail.com"))
+	                .build();
+	    }
+}
