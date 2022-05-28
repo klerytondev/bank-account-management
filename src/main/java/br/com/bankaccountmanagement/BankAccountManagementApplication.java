c065e14d5e32764fcab1103399c76bf57412730f
Kleryton <kleryton.dev@gmail.com>
2022-05-28 18:01:07 -0300

    update class BankAccountManagementApplication

--- src/main/java/br/com/bankaccountmanagement/BankAccountManagementApplication.java
+++ src/main/java/br/com/bankaccountmanagement/BankAccountManagementApplication.java
@@ -3,7 +3,11 @@ package br.com.bankaccountmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+//Inserido para corrigir erro de requizição do Swagger na base URL: localhost:8080/
+@EnableSwagger2
 public class BankAccountManagementApplication {
 
 	public static void main(String[] args) {
