f060afb91a641ee59982e588d709b3abaa9b0f51
Kleryton <kleryton.dev@gmail.com>
2022-05-28 14:02:36 -0300

    Created classes AccountsClockRequestDto and DepositRequestDto

---
+++ src/main/java/br/com/bankaccountmanagement/requestDto/AccountBlockRequestDto.java
@@ -0,0 +1,9 @@
+package br.com.bankaccountmanagement.requestDto;
+
+import java.io.Serializable;
+
+public class AccountBlockRequestDto implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/br/com/bankaccountmanagement/requestDto/ExtractByPeriodRequestDto.java
@@ -0,0 +1,10 @@
+package br.com.bankaccountmanagement.requestDto;
+
+import java.io.Serializable;
+
+public class ExtractByPeriodRequestDto implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
