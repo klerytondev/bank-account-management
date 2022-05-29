90556e43037a0f18834878bf902e6bb43bc0481a
Kleryton <kleryton.dev@gmail.com>
2022-05-29 08:49:01 -0300

    Implemented class AccountBlock

--- src/main/java/br/com/bankaccountmanagement/requestDto/AccountBlockRequestDto.java
+++ src/main/java/br/com/bankaccountmanagement/requestDto/AccountBlockRequestDto.java
@@ -2,7 +2,28 @@ package br.com.bankaccountmanagement.requestDto;
 
 import java.io.Serializable;
 
-public class AccountBlockRequestDto implements Serializable{
+import javax.validation.constraints.Pattern;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class AccountBlockRequestDto implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	@ApiModelProperty(notes = "validar a entgrada para bloqueio da account. Só é permitido a entrada: BLOCK.")
+	@Pattern(regexp = "BLOCK", message = "{campo.block.obrigatorio}")
+	private String accountBlock;
+
+	public AccountBlockRequestDto(
+			@Pattern(regexp = "BLOCK", message = "{campo.block.obrigatorio}") String accountBlock) {
+		this.accountBlock = accountBlock;
+	}
+
+	public String getAccountBlock() {
+		return accountBlock;
+	}
+
+	public void setAccountBlock(String accountBlock) {
+		this.accountBlock = accountBlock;
+	}
 
 }
