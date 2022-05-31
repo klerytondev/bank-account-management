00ad97fb331d9afa7b45f5319982bcdd3cb88f83
Kleryton <kleryton.dev@gmail.com>
2022-05-30 23:18:58 -0300

    update port AWS

--- src/main/java/br/com/bankaccountmanagement/configs/SwaggerConfig.java
+++ src/main/java/br/com/bankaccountmanagement/configs/SwaggerConfig.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author: Kleryton de souza
@@ -17,7 +16,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 		      
 	    @Bean
