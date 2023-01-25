48be8b1b745ba9aaa69314589dc0b91eeae26c97
Kleryton Souza <kleryton.dev@gmail.com>
2023-01-25 20:36:08 -0300

    Update project

--- src/main/java/br/com/bankaccountmanagement/models/AccountModel.java
+++ src/main/java/br/com/bankaccountmanagement/models/AccountModel.java
@@ -31,132 +31,133 @@ import br.com.bankaccountmanagement.models.enums.ActiveFlag;
 @Entity
 @Table(name = "TB_ACCOUNT")
 public class AccountModel implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idAccount;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAccount;
 
-	@Column(nullable = false)
-	private Double balance = 0.0;
+    @Column(nullable = false)
+    private Double balance = 0.0;
 
-	@Column(nullable = false)
-	private Double withdrawalLimit = 0.0;
+    @Column(nullable = false)
+    private Double withdrawalLimit = 0.0;
 
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private ActiveFlag activeFlag;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ActiveFlag activeFlag;
 
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private AccountType accountType;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AccountType accountType;
 
-	@Column
-	private LocalDate createdDate;
+    @Column
+    private LocalDate createdDate;
 
-	// Uma account pode ter várias transações @oneToMany
-	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name = "account_id", foreignKey = @ForeignKey(name = "fk_account"))
-	private Set<TransactionModel> transactionModels;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "people_id", foreignKey = @ForeignKey(name = "fk_people"))
-	private PeopleModel peopleModel;
+    // Uma account pode ter várias transações @oneToMany
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "account_id", foreignKey = @ForeignKey(name = "fk_account"))
+    private Set<TransactionModel> transactionModels;
 
-	public AccountModel() {
-		this.activeFlag = ActiveFlag.ACTIVE;
-		this.createdDate = LocalDate.now();
-	}
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "people_id", foreignKey = @ForeignKey(name = "fk_people"))
+    private PeopleModel peopleModel;
 
-	public AccountModel(Double balance, Double withdrawalLimit, ActiveFlag activeFlag, AccountType accountType,
-			LocalDate createdDate, Set<TransactionModel> transactionModels, PeopleModel peopleModel) {
-		this.balance = balance;
-		this.withdrawalLimit = withdrawalLimit;
-		this.activeFlag = activeFlag;
-		this.accountType = accountType;
-		this.transactionModels = transactionModels;
-		this.peopleModel = peopleModel;
-	}
+    public AccountModel() {
+        this.activeFlag = ActiveFlag.ACTIVE;
+        this.createdDate = LocalDate.now();
+    }
 
-	public Double getBalance() {
-		return balance;
-	}
+    public AccountModel(Double balance, Double withdrawalLimit, ActiveFlag activeFlag, AccountType accountType,
+                        LocalDate createdDate, Set<TransactionModel> transactionModels, PeopleModel peopleModel) {
+        this.balance = balance;
+        this.withdrawalLimit = withdrawalLimit;
+        this.activeFlag = activeFlag;
+        this.accountType = accountType;
+        this.transactionModels = transactionModels;
+        this.peopleModel = peopleModel;
+    }
 
-	public void setBalance(Double balance) {
-		this.balance = balance;
-	}
+    public Double getBalance() {
+        return balance;
+    }
 
-	public Double getWithdrawalLimit() {
-		return withdrawalLimit;
-	}
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
 
-	public void setWithdrawalLimit(Double withdrawalLimit) {
-		this.withdrawalLimit = withdrawalLimit;
-	}
+    public Double getWithdrawalLimit() {
+        return withdrawalLimit;
+    }
 
-	public ActiveFlag getActiveFlag() {
-		return activeFlag;
-	}
+    public void setWithdrawalLimit(Double withdrawalLimit) {
+        this.withdrawalLimit = withdrawalLimit;
+    }
 
-	public void setActiveFlag(ActiveFlag activeFlag) {
-		this.activeFlag = activeFlag;
-	}
+    public ActiveFlag getActiveFlag() {
+        return activeFlag;
+    }
 
-	public AccountType getAccountType() {
-		return accountType;
-	}
+    public void setActiveFlag(ActiveFlag activeFlag) {
+        this.activeFlag = activeFlag;
+    }
 
-	public void setAccountType(AccountType accountType) {
-		this.accountType = accountType;
-	}
+    public AccountType getAccountType() {
+        return accountType;
+    }
 
-	public LocalDate getCreatedDate() {
-		return createdDate;
-	}
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
 
-	public Set<TransactionModel> getTransactionModels() {
-		return transactionModels;
-	}
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
 
-	public void setTransactionModels(Set<TransactionModel> transactionModels) {
-		this.transactionModels = transactionModels;
-	}
+    public Set<TransactionModel> getTransactionModels() {
+        return transactionModels;
+    }
 
-	public Long getId() {
-		return idAccount;
-	}
-	public void setIdAccount(Long idAccount) {
-		this.idAccount = idAccount;
-	}
-	
-	public void setPeople(PeopleModel people) {
-		this.peopleModel= people;
-	}
-	
-	public PeopleModel getIdPeople() {
-		return peopleModel;
-	}
+    public void setTransactionModels(Set<TransactionModel> transactionModels) {
+        this.transactionModels = transactionModels;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(accountType, activeFlag, balance, createdDate, idAccount, transactionModels,
-				withdrawalLimit);
-	}
+    public Long getId() {
+        return idAccount;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		AccountModel other = (AccountModel) obj;
-		return accountType == other.accountType && activeFlag == other.activeFlag
-				&& Objects.equals(balance, other.balance) && Objects.equals(createdDate, other.createdDate)
-				&& Objects.equals(idAccount, other.idAccount)
-				&& Objects.equals(transactionModels, other.transactionModels)
-				&& Objects.equals(withdrawalLimit, other.withdrawalLimit);
-	}
+    public void setIdAccount(Long idAccount) {
+        this.idAccount = idAccount;
+    }
+
+    public void setPeople(PeopleModel people) {
+        this.peopleModel = people;
+    }
+
+    public PeopleModel getIdPeople() {
+        return peopleModel;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountType, activeFlag, balance, createdDate, idAccount, transactionModels,
+                withdrawalLimit);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AccountModel other = (AccountModel) obj;
+        return accountType == other.accountType && activeFlag == other.activeFlag
+                && Objects.equals(balance, other.balance) && Objects.equals(createdDate, other.createdDate)
+                && Objects.equals(idAccount, other.idAccount)
+                && Objects.equals(transactionModels, other.transactionModels)
+                && Objects.equals(withdrawalLimit, other.withdrawalLimit);
+    }
 
 }
